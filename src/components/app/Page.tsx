d4c000a62a14f546c30d0fd87c014b4a55804dd4
gian <gian9889@gmail.com>
2025-11-03 16:02:45 -0600

    fix: responsive style

--- src/components/app/Page.tsx
+++ src/components/app/Page.tsx
@@ -5,7 +5,7 @@ interface PageProps extends HtmlHTMLAttributes<HTMLDivElement> {
 }
 
 export function Page(props: PageProps) {
-    return <div className={cn('flex flex-col w-screen h-screen min-h-0 overflow-auto mb-[112px] lg:pb-0', props.className)}>
+    return <div className={cn('flex flex-col w-screen h-screen min-h-0 overflow-auto mb-[112px] lg:mb-auto lg:pb-0', props.className)}>
         {props.children}
     </div>
 }
